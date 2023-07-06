228af9d1c85ae4b87cec55798c9bc9bd29a19f12
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-06 18:43:18 +0200

    somma snack2 funzionante

--- jsnack2/jsnack2.js
+++ jsnack2/jsnack2.js
@@ -1,4 +1,4 @@
-let listaNumeri = []  //array vuoto numeri
+let listaNumeri = [];  //array vuoto numeri
 
 const btnLista = document.querySelector(".confermaNum");
 
@@ -6,10 +6,30 @@ const btnLista = document.querySelector(".confermaNum");
 btnLista.addEventListener("click", function () {
     const numerInputElement = document.querySelector("[id=numeroInput]");
 
-    let singNum = numerInputElement.value 
-    console.log(singNum)
+    let singNum = parseInt(numerInputElement.value);
+    listaNumeri.push (singNum);
+    console.log(listaNumeri);
+})
 
-    listaNumeri.push (singNum)
-    console.log(listaNumeri)
+
+
+
+const btnSomma = document.querySelector(".sommaNum");
+
+btnSomma.addEventListener("click", function () {
+        let somma = 0;
+
+        for ( let i = 0 ; i <= listaNumeri.length -1 ; i++){
+
+            let numAttuale = parseInt (listaNumeri[i]);
+
+            if (i %2 !==0) {
+                numAttuale=0;
+            }
+            somma = somma + numAttuale;
+
+        }
+        console.log(somma);
+
 
 })
